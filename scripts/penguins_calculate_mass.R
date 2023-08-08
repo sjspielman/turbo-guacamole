e96a9b8bd2b74ac6e23df26216191fd08fea231e
Stephanie Spielman <stephanie.spielman@ccdatalab.org>
2023-08-08 13:15:01 -0400

    update mass script

--- scripts/penguins_calculate_mass.R
+++ scripts/penguins_calculate_mass.R
@@ -5,7 +5,8 @@ output_tsv <- here::here("results", "penguins_mass.tsv")
 
 # Create data frame of mean mass for each species
 penguin_mass <- penguins |>
-  dplyr::group_by(species) |>
+  dplyr::group_by(species, island) |>
+  dplyr::filter(year == 2008) |>
   # ignore NAs when calculating mean
   dplyr::summarize(mean_mass = mean(body_mass_g, na.rm=TRUE))
 
