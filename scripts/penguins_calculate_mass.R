32be389b476cac3b38562bb312e60134f4023d4b
Stephanie Spielman <stephanie.spielman@ccdatalab.org>
2023-08-08 13:16:13 -0400

    one grouping?

--- scripts/penguins_calculate_mass.R
+++ scripts/penguins_calculate_mass.R
@@ -5,7 +5,7 @@ output_tsv <- here::here("results", "penguins_mass.tsv")
 
 # Create data frame of mean mass for each species
 penguin_mass <- penguins |>
-  dplyr::group_by(species, island) |>
+  dplyr::group_by(species) |>
   dplyr::filter(year == 2008) |>
   # ignore NAs when calculating mean
   dplyr::summarize(mean_mass = mean(body_mass_g, na.rm=TRUE))
