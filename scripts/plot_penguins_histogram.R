920816b274926b7eb190d191e39325d7d8e2630f
Stephanie Spielman <stephanie.spielman@ccdatalab.org>
2023-08-08 12:49:42 -0400

    add ggsave to script

--- scripts/plot_penguins_histogram.R
+++ scripts/plot_penguins_histogram.R
@@ -2,8 +2,14 @@
 library(ggplot2)
 library(palmerpenguins)
 
+output_file <- here::here("plots", "penguin_histogram.png")
+
 # Make histogram of penguin bill depth --------
-ggplot(penguins) +
+penguin_histogram <- ggplot(penguins) +
   aes(x = bill_depth_mm) +
   geom_histogram() +
   labs(x = "Penguin bill depth (mm)")
+
+# Export plot
+ggsave(filename = output_file,
+       plot = penguin_histogram)
